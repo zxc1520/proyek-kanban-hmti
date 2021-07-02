048e1e9112edacc0231eadede7a0d9144ea9b5e2
zxc1520 <afifghulam21@gmail.com>
2021-07-02 20:37:59 +0700

    ngerapihin card kanban dengan modal

--- application/views/admin/kanban.php
+++ application/views/admin/kanban.php
@@ -20,7 +20,6 @@
                     </div> -->
                 </div>
                 <h5 class="card-title">Todo</h5>
-                <h6 class="card-subtitle text-muted">Nam pretium turpis et arcu. Duis arcu tortor.</h6>
             </div>
             <div class="card-body p-3">
 
@@ -32,13 +31,15 @@
                                 <span class="custom-control-label"></span>
                             </label>
                         </div>
+                        <h4>Fasilitas</h4>
                         <p>Curabitur ligula sapien, tincidunt non, euismod vitae, posuere imperdiet, leo. Maecenas malesuada.</p>
-                        <div class="float-right mt-n1">
+                        <!-- <div class="float-right mt-n1">
                             <img src="https://bootdey.com/img/Content/avatar/avatar6.png" width="32" height="32" class="rounded-circle" alt="Avatar">
-                        </div>
+                        </div> -->
                         <a class="btn btn-outline-primary btn-sm" href="#">View</a>
                     </div>
                 </div>
+                <hr class="my-4">
                 <div class="card mb-3 bg-light">
                     <div class="card-body p-3">
                         <div class="float-right mr-n2">
@@ -47,13 +48,15 @@
                                 <span class="custom-control-label"></span>
                             </label>
                         </div>
+                        <h4>Toleransi Waktu</h4>
                         <p>Nam pretium turpis et arcu. Duis arcu tortor, suscipit eget, imperdiet nec, imperdiet iaculis, ipsum.</p>
-                        <div class="float-right mt-n1">
+                        <!-- <div class="float-right mt-n1">
                             <img src="https://bootdey.com/img/Content/avatar/avatar1.png" width="32" height="32" class="rounded-circle" alt="Avatar">
-                        </div>
+                        </div> -->
                         <a class="btn btn-outline-primary btn-sm" href="#">View</a>
                     </div>
                 </div>
+                <hr class="my-4">
                 <div class="card mb-3 bg-light">
                     <div class="card-body p-3">
                         <div class="float-right mr-n2">
@@ -62,13 +65,15 @@
                                 <span class="custom-control-label"></span>
                             </label>
                         </div>
+                        <h4>Bantuan</h4>
                         <p>Aenean posuere, tortor sed cursus feugiat, nunc augue blandit nunc, eu sollicitudin urna dolor sagittis.</p>
-                        <div class="float-right mt-n1">
+                        <!-- <div class="float-right mt-n1">
                             <img src="https://bootdey.com/img/Content/avatar/avatar2.png" width="32" height="32" class="rounded-circle" alt="Avatar">
-                        </div>
+                        </div> -->
                         <a class="btn btn-outline-primary btn-sm" href="#">View</a>
                     </div>
                 </div>
+                <hr class="my-4">
                 <div class="card mb-3 bg-light">
                     <div class="card-body p-3">
                         <div class="float-right mr-n2">
@@ -77,13 +82,15 @@
                                 <span class="custom-control-label"></span>
                             </label>
                         </div>
+                        <h4>Kuota</h4>
                         <p>In hac habitasse platea dictumst. Curabitur at lacus ac velit ornare lobortis. Curabitur a felis tristique.</p>
-                        <div class="float-right mt-n1">
+                        <!-- <div class="float-right mt-n1">
                             <img src="https://bootdey.com/img/Content/avatar/avatar3.png" width="32" height="32" class="rounded-circle" alt="Avatar">
-                        </div>
+                        </div> -->
                         <a class="btn btn-outline-primary btn-sm" href="#">View</a>
                     </div>
                 </div>
+                <hr class="my-4">
                 <div class="card mb-3 bg-light">
                     <div class="card-body p-3">
                         <div class="float-right mr-n2">
@@ -92,15 +99,47 @@
                                 <span class="custom-control-label"></span>
                             </label>
                         </div>
+                        <h4>Pengajaran</h4>
                         <p>Nam pretium turpis et arcu. Duis arcu tortor, suscipit eget, imperdiet nec, imperdiet iaculis, ipsum.</p>
-                        <div class="float-right mt-n1">
+                        <!-- <div class="float-right mt-n1">
                             <img src="https://bootdey.com/img/Content/avatar/avatar4.png" width="32" height="32" class="rounded-circle" alt="Avatar">
-                        </div>
+                        </div> -->
                         <a class="btn btn-outline-primary btn-sm" href="#">View</a>
                     </div>
                 </div>
 
-                <a href="#" class="btn btn-primary btn-block">Add new</a>
+                <button type="button" class="btn btn-primary btn-block" data-toggle="modal" data-target="#exampleModal">
+                    Add New
+                </button>
+
+                <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                    <div class="modal-dialog">
+                        <div class="modal-content">
+                        <div class="modal-header">
+                            <h5 class="modal-title" id="exampleModalLabel">Todo</h5>
+                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                            <span aria-hidden="true">&times;</span>
+                            </button>
+                        </div>
+                        <div class="modal-body">
+                            <form action="">
+                                <div class="form-group">
+                                    <label for="name">Name</label>
+                                    <input type="text" id="name" class="form-control">
+                                </div>
+                                <div class="form-group">
+                                    <label for="detail">Detail</label>
+                                    <input type="text" id="detail" class="form-control">
+                                </div>
+                            </form>
+                        </div>
+                        <div class="modal-footer">
+                            <button type="button" class="btn btn-secondary" data-dismiss="modal">Tutup</button>
+                            <button type="button" class="btn btn-primary">Simpan</button>
+                        </div>
+                        </div>
+                    </div>
+                </div>
 
             </div>
         </div>
@@ -127,7 +166,6 @@
                     </div> -->
                 </div>
                 <h5 class="card-title">In Progress</h5>
-                <h6 class="card-subtitle text-muted">Nam pretium turpis et arcu. Duis arcu tortor.</h6>
             </div>
             <div class="card-body">
 
@@ -139,13 +177,15 @@
                                 <span class="custom-control-label"></span>
                             </label>
                         </div>
+                        <h4>Pengumpulan tugas</h4>
                         <p>Curabitur ligula sapien, tincidunt non, euismod vitae, posuere imperdiet, leo. Maecenas malesuada.</p>
-                        <div class="float-right mt-n1">
+                        <!-- <div class="float-right mt-n1">
                             <img src="https://bootdey.com/img/Content/avatar/avatar6.png" width="32" height="32" class="rounded-circle" alt="Avatar">
-                        </div>
+                        </div> -->
                         <a class="btn btn-outline-primary btn-sm" href="#">View</a>
                     </div>
                 </div>
+                <hr class="my-4">
                 <div class="card mb-3 bg-light">
                     <div class="card-body p-3">
                         <div class="float-right mr-n2">
@@ -154,13 +194,15 @@
                                 <span class="custom-control-label"></span>
                             </label>
                         </div>
+                        <h4>LMS Sering Error</h4>
                         <p>Aenean posuere, tortor sed cursus feugiat, nunc augue blandit nunc, eu sollicitudin urna dolor sagittis.</p>
-                        <div class="float-right mt-n1">
+                        <!-- <div class="float-right mt-n1">
                             <img src="https://bootdey.com/img/Content/avatar/avatar7.png" width="32" height="32" class="rounded-circle" alt="Avatar">
-                        </div>
+                        </div> -->
                         <a class="btn btn-outline-primary btn-sm" href="#">View</a>
                     </div>
                 </div>
+                <hr class="my-4">
                 <div class="card mb-3 bg-light">
                     <div class="card-body p-3">
                         <div class="float-right mr-n2">
@@ -169,13 +211,15 @@
                                 <span class="custom-control-label"></span>
                             </label>
                         </div>
+                        <h4>Kendala Jaringan</h4>
                         <p>Nam pretium turpis et arcu. Duis arcu tortor, suscipit eget, imperdiet nec, imperdiet iaculis, ipsum.</p>
-                        <div class="float-right mt-n1">
+                        <!-- <div class="float-right mt-n1">
                             <img src="https://bootdey.com/img/Content/avatar/avatar8.png" width="32" height="32" class="rounded-circle" alt="Avatar">
-                        </div>
+                        </div> -->
                         <a class="btn btn-outline-primary btn-sm" href="#">View</a>
                     </div>
                 </div>
+                <hr class="my-4">
                 <div class="card mb-3 bg-light">
                     <div class="card-body p-3">
                         <div class="float-right mr-n2">
@@ -184,15 +228,47 @@
                                 <span class="custom-control-label"></span>
                             </label>
                         </div>
+                        <h4>Terlambat</h4>
                         <p>In hac habitasse platea dictumst. Curabitur at lacus ac velit ornare lobortis. Curabitur a felis tristique.</p>
-                        <div class="float-right mt-n1">
+                        <!-- <div class="float-right mt-n1">
                             <img src="https://bootdey.com/img/Content/avatar/avatar1.png" width="32" height="32" class="rounded-circle" alt="Avatar">
-                        </div>
+                        </div> -->
                         <a class="btn btn-outline-primary btn-sm" href="#">View</a>
                     </div>
                 </div>
 
-                <a href="#" class="btn btn-primary btn-block">Add new</a>
+                <button type="button" class="btn btn-primary btn-block" data-toggle="modal" data-target="#exampleModal">
+                    Add New
+                </button>
+
+                <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                    <div class="modal-dialog">
+                        <div class="modal-content">
+                        <div class="modal-header">
+                            <h5 class="modal-title" id="exampleModalLabel">On Process</h5>
+                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                            <span aria-hidden="true">&times;</span>
+                            </button>
+                        </div>
+                        <div class="modal-body">
+                            <form action="">
+                                <div class="form-group">
+                                    <label for="name">Name</label>
+                                    <input type="text" id="name" class="form-control">
+                                </div>
+                                <div class="form-group">
+                                    <label for="detail">Detail</label>
+                                    <input type="text" id="detail" class="form-control">
+                                </div>
+                            </form>
+                        </div>
+                        <div class="modal-footer">
+                            <button type="button" class="btn btn-secondary" data-dismiss="modal">Tutup</button>
+                            <button type="button" class="btn btn-primary">Simpan</button>
+                        </div>
+                        </div>
+                    </div>
+                </div>
 
             </div>
         </div>
@@ -295,8 +371,8 @@
                         </div>
                     </div> -->
                 </div>
-                <h5 class="card-title">Complete</h5>
-                <h6 class="card-subtitle text-muted">Nam pretium turpis et arcu. Duis arcu tortor.</h6>
+                <h5 class="card-title">Done</h5>
+               
             </div>
             <div class="card-body">
 
@@ -308,13 +384,15 @@
                                 <span class="custom-control-label"></span>
                             </label>
                         </div>
+                        <h4>Jarang Masuk Kelas</h4>
                         <p>Nam pretium turpis et arcu. Duis arcu tortor, suscipit eget, imperdiet nec, imperdiet iaculis, ipsum.</p>
-                        <div class="float-right mt-n1">
+                        <!-- <div class="float-right mt-n1">
                             <img src="https://bootdey.com/img/Content/avatar/avatar6.png" width="32" height="32" class="rounded-circle" alt="Avatar">
-                        </div>
+                        </div> -->
                         <a class="btn btn-outline-primary btn-sm" href="#">View</a>
                     </div>
                 </div>
+                <hr class="my-4">
                 <div class="card mb-3 bg-light">
                     <div class="card-body p-3">
                         <div class="float-right mr-n2">
@@ -323,13 +401,15 @@
                                 <span class="custom-control-label"></span>
                             </label>
                         </div>
+                        <h4>LMS Lemot</h4>
                         <p>In hac habitasse platea dictumst. Curabitur at lacus ac velit ornare lobortis. Curabitur a felis tristique.</p>
-                        <div class="float-right mt-n1">
+                        <!-- <div class="float-right mt-n1">
                             <img src="https://bootdey.com/img/Content/avatar/avatar7.png" width="32" height="32" class="rounded-circle" alt="Avatar">
-                        </div>
+                        </div> -->
                         <a class="btn btn-outline-primary btn-sm" href="#">View</a>
                     </div>
                 </div>
+                <hr class="my-4">
                 <div class="card mb-3 bg-light">
                     <div class="card-body p-3">
                         <div class="float-right mr-n2">
@@ -338,13 +418,12 @@
                                 <span class="custom-control-label"></span>
                             </label>
                         </div>
+                        <h4>Bantuan Linkaja</h4>
                         <p>Aenean posuere, tortor sed cursus feugiat, nunc augue blandit nunc, eu sollicitudin urna dolor sagittis.</p>
-                        <div class="float-right mt-n1">
-                            <img src="https://bootdey.com/img/Content/avatar/avatar8.png" width="32" height="32" class="rounded-circle" alt="Avatar">
-                        </div>
                         <a class="btn btn-outline-primary btn-sm" href="#">View</a>
                     </div>
                 </div>
+                <hr class="my-4">
                 <div class="card mb-3 bg-light">
                     <div class="card-body p-3">
                         <div class="float-right mr-n2">
@@ -353,14 +432,44 @@
                                 <span class="custom-control-label"></span>
                             </label>
                         </div>
+                        <h4>Pengumpulan Tugas Kuliah</h4>
                         <p>Curabitur ligula sapien, tincidunt non, euismod vitae, posuere imperdiet, leo. Maecenas malesuada.</p>
-                        <div class="float-right mt-n1">
-                            <img src="https://bootdey.com/img/Content/avatar/avatar1.png" width="32" height="32" class="rounded-circle" alt="Avatar">
-                        </div>
                         <a class="btn btn-outline-primary btn-sm" href="#">View</a>
                     </div>
                 </div>
-                <a href="#" class="btn btn-primary btn-block">Add new</a>
+                <button type="button" class="btn btn-primary btn-block" data-toggle="modal" data-target="#exampleModal">
+                    Add New
+                </button>
+
+                <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                    <div class="modal-dialog">
+                        <div class="modal-content">
+                        <div class="modal-header">
+                            <h5 class="modal-title" id="exampleModalLabel">Done</h5>
+                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                            <span aria-hidden="true">&times;</span>
+                            </button>
+                        </div>
+                        <div class="modal-body">
+                            <form action="">
+                                <div class="form-group">
+                                    <label for="name">Name</label>
+                                    <input type="text" id="name" class="form-control">
+                                </div>
+                                <div class="form-group">
+                                    <label for="detail">Detail</label>
+                                    <input type="text" id="detail" class="form-control">
+                                </div>
+                            </form>
+                        </div>
+                        <div class="modal-footer">
+                            <button type="button" class="btn btn-secondary" data-dismiss="modal">Tutup</button>
+                            <button type="button" class="btn btn-primary">Simpan</button>
+                        </div>
+                        </div>
+                    </div>
+                </div>
+                <!-- End of modal -->
             </div>
         </div>
     </div>
